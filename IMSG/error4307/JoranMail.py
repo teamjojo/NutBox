7b196bf73404275fb36a6e3fe3c91960307ac06a
teamjojo <jojoosinga@gmail.com>
2018-11-04 19:57:27 +0100

    updated

--- IMSG/error4307/JoranMail.py
+++ IMSG/error4307/JoranMail.py
@@ -3,7 +3,7 @@ fromaddr = 'tienerschoolnut@gmail.com'
 toaddrs  = 'jojoosinga@gmail.com'
 msg = 'De NutBox is oververhit IMSG in actief de brakes zijn actief(stroom is uit) de Arduino heeft nog 20 minuten batterij Joran en Indy komen zo spoedig mogelijk'
 username = 'tienerschoolnut@gmail.com'
-password = 'J0r@n2006'
+password = '***'
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.ehlo()
 server.starttls()
